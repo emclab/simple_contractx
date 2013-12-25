587f079f4776542cc8b91cb74cb0172111d1320b
jc <emclab2011@gmail.com>
2013-12-25 09:27:26 -0600

    JC: more CN. minor change in views.

--- app/controllers/simple_contractx/contracts_controller.rb
+++ app/controllers/simple_contractx/contracts_controller.rb
@@ -53,8 +53,8 @@ module SimpleContractx
     
     protected
     def load_parent_record
-      @project = TemplateTaskx.project_class.find_by_id(params[:project_id]) if params[:project_id].present? 
-      @project = TemplateTaskx.project_class.find_by_id(SimpleContractx::Contract.find_by_id(params[:id]).project_id) if params[:id].present? 
+      @project = SimpleContractx.project_class.find_by_id(params[:project_id]) if params[:project_id].present? 
+      @project = SimpleContractx.project_class.find_by_id(SimpleContractx::Contract.find_by_id(params[:id]).project_id) if params[:id].present? 
     end
     
   end
